ac2a7e1bfd468eb9e0625facccae8f13a7961c90
Luanmartins <luan_18martins@hotmail.com>
2024-08-28 19:08:24 -0300

    feat: finalizando layoutizacao

---
+++ resources/views/livewire/user/login.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <form>
+        teste
+        <p class="text-light">Por favor, logue na sua conta</p>
+
+        <div class="form-outline mb-4">
+            <input type="email" id="email" class="form-control" placeholder="Digite seu email" />
+            <label class="form-label" for="email">E-mail</label>
+        </div>
+
+        <div class="form-outline mb-4">
+            <input type="password" id="password" class="form-control" />
+            <label class="form-label" for="password">Senha</label>
+        </div>
+
+        <div class="text-center pt-1 mb-5 pb-1">
+            <button class="btn btn-warning btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
+                in</button>
+            <a class="text-light" href="#!">Forgot password?</a>
+        </div>
+
+        <div class="d-flex align-items-center justify-content-center pb-4">
+            <p class="mb-0 me-2 text-light">Crie sua conta</p>
+            <button type="button" class="btn btn-warning gradient-custom-2">Criar
+                Nova</button>
+        </div>
+
+    </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Livewire\Teste;
+use App\Livewire\User\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste', Teste::class);
+Route::get('/login', Login::class);
